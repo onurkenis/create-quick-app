a4c16a8c870647f01ff1eee1e80edd3757f0f206
onurkenis <kenisonur@yandex.com>
2020-04-11 11:08:00 +0300

    refactor(utils): prettify code style

--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,15 @@
-export const getManifestFilePath = (options) => `${options.targetDirectory}/src/manifest.json`; 
+export const getManifestFilePath = (options) => `${options.targetDirectory}/src/manifest.json`;
 
 export const getUxFilePath = (options) => `${options.targetDirectory}/src/Hello/hello.ux`;
 
-export const getTargetDirectory = (options) => `${process.cwd()}/${options.packageName.toLowerCase()}`;
+export const getTargetDirectory = (options) =>
+  `${process.cwd()}/${options.packageName.toLowerCase()}`;
 
 export const getUpdatedManifest = (options) => {
-    const manifest = JSON.parse(manifestJson);
-        
-    manifest.package = options.packageName;
-    manifest.name = options.appName;
+  const manifest = JSON.parse(manifestJson);
 
-    return JSON.stringify(manifest, null, 2);
-}
+  manifest.package = options.packageName;
+  manifest.name = options.appName;
+
+  return JSON.stringify(manifest, null, 2);
+};
