69eddcb2271615a674a9eace5ad586d5e61eb34f
onurkenis <kenisonur@yandex.com>
2020-04-11 11:08:56 +0300

    refactor(cli): prettify code style

--- src/cli.js
+++ src/cli.js
@@ -3,64 +3,64 @@ import inquirer from 'inquirer';
 import { createProject } from './main';
 
 function parseArgumentsIntoOptions(rawArgs) {
-    const args = arg(
-        {
-            '--appName': String,
-            '--packageName': String,
-            '--url': String,
-        },
-        {
-            argv: rawArgs.slice(2),
-        }
-    );
+  const args = arg(
+    {
+      '--appName': String,
+      '--packageName': String,
+      '--url': String,
+    },
+    {
+      argv: rawArgs.slice(2),
+    },
+  );
 
-    return {
-        appName: args['--appName'] || false,
-        packageName: args['--packageName'] || false,
-        sourceUrl: args['--url'] || false,
-    };
+  return {
+    appName: args['--appName'] || false,
+    packageName: args['--packageName'] || false,
+    sourceUrl: args['--url'] || false,
+  };
 }
 
 async function promptForMissingOptions(options) {
-    const questions = [];
+  const questions = [];
 
-    if (!options.appName) {
-        questions.push({
-            type: 'input',
-            name: 'appName',
-            message: 'Enter your app name:',
-        });
-    }
+  if (!options.appName) {
+    questions.push({
+      type: 'input',
+      name: 'appName',
+      message: 'Enter your app name:',
+    });
+  }
 
-    if (!options.packageName) {
-        questions.push({
-            type: 'input',
-            name: 'packageName',
-            message: 'Enter your package name:',
-            default: 'com.onurkenis.quickapp'
-        });
-    }
+  if (!options.packageName) {
+    questions.push({
+      type: 'input',
+      name: 'packageName',
+      message: 'Enter your package name:',
+      default: 'com.onurkenis.quickapp',
+    });
+  }
 
-    if (!options.sourceUrl) {
-        questions.push({
-            type: 'input',
-            name: 'sourceUrl',
-            message: 'Enter your source url:',
-        });
-    }
+  if (!options.sourceUrl) {
+    questions.push({
+      type: 'input',
+      name: 'sourceUrl',
+      message: 'Enter your source url:',
+    });
+  }
 
-    const answers = await inquirer.prompt(questions);
-    return {
-        ...options,
-        appName: options.appName || answers.appName,
-        packageName: options.packageName || answers.packageName,
-        sourceUrl: options.sourceUrl || answers.sourceUrl
-    };
+  const answers = await inquirer.prompt(questions);
+  return {
+    ...options,
+    appName: options.appName || answers.appName,
+    packageName: options.packageName || answers.packageName,
+    sourceUrl: options.sourceUrl || answers.sourceUrl,
+  };
 }
 
 export async function cli(args) {
-    let options = parseArgumentsIntoOptions(args);
-    options = await promptForMissingOptions(options);
+  let options = parseArgumentsIntoOptions(args);
+  options = await promptForMissingOptions(options);
 
-    await createProject(options);
+  await createProject(options);
 }
