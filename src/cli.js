04730fafcc5ee69d90434f7369c1c4f183d664ab
onurkenis <kenisonur@yandex.com>
2020-04-11 10:05:24 +0300

    feat: parse arguments into options

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,4 @@
 import arg from 'arg';
-import inquirer from 'inquirer';
-import { createProject } from './main';
 
 function parseArgumentsIntoOptions(rawArgs) {
     const args = arg(
@@ -13,7 +11,7 @@ function parseArgumentsIntoOptions(rawArgs) {
             argv: rawArgs.slice(2),
         }
     );
-    console.log(args);
+    
     return {
         appName: args['--appName'] || false,
         packageName: args['--packageName'] || false,
@@ -21,46 +19,7 @@ function parseArgumentsIntoOptions(rawArgs) {
     };
 }
 
-async function promptForMissingOptions(options) {
-    const questions = [];
-
-    if (!options.appName) {
-        questions.push({
-            type: 'input',
-            name: 'appName',
-            message: 'What is your app name?',
-        });
-    }
-
-    if (!options.packageName) {
-        questions.push({
-            type: 'input',
-            name: 'packageName',
-            message: 'What is your package name?',
-            default: 'com.onurkenis.quickapp'
-        });
-    }
-
-    if (!options.sourceUrl) {
-        questions.push({
-            type: 'input',
-            name: 'sourceUrl',
-            message: 'What is your source url?',
-        });
-    }
-
-    const answers = await inquirer.prompt(questions);
-    return {
-        ...options,
-        appName: options.appName || answers.appName,
-        packageName: options.packageName || answers.packageName,
-        sourceUrl: options.sourceUrl || answers.sourceUrl
-    };
-}
-
 export async function cli(args) {
-    let options = parseArgumentsIntoOptions(args);
-    options = await promptForMissingOptions(options);
-    
-    await createProject(options);
+    const options = parseArgumentsIntoOptions(args);
+    console.log(options)
 }
