a4e12efaa86c78817ae4a99f1da1abd0b504ffb3
onurkenis <kenisonur@yandex.com>
2020-04-11 10:16:52 +0300

    feat: add tasks to createProject

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import chalk from 'chalk';
 import fs from 'fs';
 import path from 'path';
+import Listr from 'listr';
 
 export async function createProject(options) {
     options = {
@@ -23,6 +24,22 @@ export async function createProject(options) {
         process.exit(1);
     }
 
+    const tasks = new Listr([
+        {
+            title: 'Copy project files',
+            task: () => console.log("COPY_PROJECT_FILE"),
+        },
+        {
+            title: 'Uptade manifest.json',
+            task: () => console.log("UPDATE_MANIFEST_FILE"),
+        },
+        {
+            title: 'Uptade hello.ux',
+            task: () => console.log("UPDATE_HELLO_UX"),
+        }
+    ]);
+
+    await tasks.run();
     console.log('%s Project ready', chalk.green.bold('DONE'));
     return true;
 }
